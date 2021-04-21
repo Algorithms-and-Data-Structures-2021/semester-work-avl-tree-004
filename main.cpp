c376a21e2dda35c483472c8f421b795a20a48894
Semen Sokolov <simionsimion@yandex.ru>
2021-04-21 17:09:47 +0300

    updated README.md

--- main.cpp
+++ main.cpp
@@ -1,20 +1,19 @@
 #include <iostream>
 
 #include "AVLTree.hpp"
+using namespace itis;
 using namespace std;
+  int main(int /*argc*/, char* /*argv*/[]) {
+    AVLTree<std::string> tree;
+    tree.insert("a");
+    tree.insert("b");
+    tree.insert("c");
+    tree.insert("d");
+    tree.insert("e");
+    tree.insert("f");
+    tree.insert("g");
 
-int main(int  /*argc*/, char * /*argv*/[]) {
-  AVLTree<std::string> tree;
-  tree.insert("a");
-  tree.insert("b");
-  tree.insert("c");
-  tree.insert("d");
-  tree.insert("e");
-
-
-  tree.display();
-
-
-
-  std::cout<<tree.find("c");
-}
+    tree.display();
+    cout<<tree.find_max();
+    return 0;
+  }
