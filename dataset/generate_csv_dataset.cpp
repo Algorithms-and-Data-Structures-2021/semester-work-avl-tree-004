1614b8e44aedbfbe78f4e353e5b5b5a1f9eeeff1
Semen Sokolov <simionsimion@yandex.ru>
2021-04-24 19:13:39 +0300

    Data generator for benchmark

--- dataset/generate_csv_dataset.cpp
+++ dataset/generate_csv_dataset.cpp
@@ -9,49 +9,68 @@
 using namespace std;
 
 // абсолютный путь до набора данных
-static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR};
+static constexpr auto kDatasetPathInsert = string_view{PROJECT_DATASET_DIR_INSERT};
+static constexpr auto kDatasetPathFind = string_view{PROJECT_DATASET_DIR_FIND};
+static constexpr auto kDatasetPathErase = string_view{PROJECT_DATASET_DIR_ERASE};
 
-int main(int argc, char** argv) {
+int main() {
+  const auto pathToFind = string(kDatasetPathFind);
+  const auto pathToErase = string(kDatasetPathErase);
+  const auto pathToInsert = string(kDatasetPathInsert);
+  cout << "Dataset pathToInsert: " << pathToInsert << endl;
+  cout << "Dataset pathToFind: " << pathToFind << endl;
+  cout << "Dataset pathToErase: " << pathToErase << endl;
 
-  // Tip 1: можете передать путь до входного/выходного файла в качестве аргумента
-  for (int index = 0; index < argc; index++) {
-    cout << "Arg: " << argv[index] << '\n';
-  }
+  vector<string> output_streams;
 
-  const auto path = string(kDatasetPath);  // конвертация string_view в string
-  cout << "Dataset path: " << path << endl;
+  output_streams.emplace_back("/100.csv");
+  output_streams.emplace_back("/500.csv");
+  output_streams.emplace_back("/1000.csv");
+  output_streams.emplace_back("/5k.csv");
+  output_streams.emplace_back("/10k.csv");
+  output_streams.emplace_back("/25k.csv");
+  output_streams.emplace_back("/50k.csv");
+  output_streams.emplace_back("/100k.csv");
+  output_streams.emplace_back("/250k.csv");
+  output_streams.emplace_back("/500k.csv");
+  output_streams.emplace_back("/750k.csv");
+  output_streams.emplace_back("/1kk.csv");
+  output_streams.emplace_back("/5kk.csv");
 
-  // Пример: чтение набора данных
-  auto input_stream = ifstream(path + "/dataset-example.csv");
+  vector<int> integers = {100, 500, 1000, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 750000, 1000000, 5000000};
 
-  if (input_stream) {
-    int line_number = 0;
+  while (!integers.empty()) {
+    auto outputStreamForFind = ofstream(pathToFind + output_streams.front(), ios::ios_base::app);
+    auto outputStreamForErase = ofstream(pathToErase + output_streams.front(), ios::ios_base::app);
+    auto outputStreamForInsert = ofstream(pathToInsert + output_streams.front(), ios::ios_base::app);
 
-    // построчное чтение
-    for (string line; getline(input_stream, line); /* ... */) {
-      auto ss = stringstream(line);  // оборачиваем строку в объект "поток"
+    const auto seed = chrono::system_clock::now().time_since_epoch().count();
+    auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
+    auto dist = uniform_int_distribution(0, 1000);  // равновероятное распределение генерируемых чисел
 
-      for (string token; getline(ss, token, ','); /* ... */) {
-        cout << "Token: [" << token << "] at line " << line_number << '\n';
+    if (outputStreamForFind) {
+      for (int counter = 0; counter < integers.front(); counter++) {
+        outputStreamForFind << dist(engine) << ',';
       }
-
-      cout << line << '\n';
-      line_number++;
+      outputStreamForFind << dist(engine) << '\n';
     }
-  }
 
-  // Пример: генерация набора данных
-  auto output_stream = ofstream(path + "/dataset-generated.csv", ios::ios_base::app);
-
-  const auto seed = chrono::system_clock::now().time_since_epoch().count();
-  auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
-  auto dist = uniform_int_distribution(0, 10);  // равновероятное распределение генерируемых чисел
-
-  if (output_stream) {
-    for (int counter = 0; counter < 10; counter++) {
-      output_stream << dist(engine) << ',';
+    if (outputStreamForErase) {
+      for (int counter = 0; counter < integers.front(); counter++) {
+        outputStreamForErase << dist(engine) << ',';
+      }
+      outputStreamForErase << dist(engine) << '\n';
     }
-    output_stream << dist(engine) << '\n';
+
+    if (outputStreamForInsert) {
+      for (int counter = 0; counter < integers.front(); counter++) {
+        outputStreamForInsert << dist(engine) << ',';
+      }
+      outputStreamForInsert << dist(engine) << '\n';
+    }
+
+    integers.erase(integers.begin());
+    output_streams.erase(output_streams.begin());
   }
 
   return 0;
