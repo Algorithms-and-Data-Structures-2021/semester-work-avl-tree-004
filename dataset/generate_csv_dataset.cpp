f92704480d2ad795551e08f20255a2c03ab00bfb
Semen Sokolov <simionsimion@yandex.ru>
2021-04-25 19:14:28 +0300

    benchmarks and write results in files

--- dataset/generate_csv_dataset.cpp
+++ dataset/generate_csv_dataset.cpp
@@ -25,18 +25,15 @@ int main() {
   output_streams.emplace_back("/100.csv");
   output_streams.emplace_back("/500.csv");
   output_streams.emplace_back("/1000.csv");
-  output_streams.emplace_back("/5k.csv");
-  output_streams.emplace_back("/10k.csv");
-  output_streams.emplace_back("/25k.csv");
-  output_streams.emplace_back("/50k.csv");
-  output_streams.emplace_back("/100k.csv");
-  output_streams.emplace_back("/250k.csv");
-  output_streams.emplace_back("/500k.csv");
-  output_streams.emplace_back("/750k.csv");
-  output_streams.emplace_back("/1kk.csv");
-  output_streams.emplace_back("/5kk.csv");
+  output_streams.emplace_back("/5000.csv");
+  output_streams.emplace_back("/10000.csv");
+  output_streams.emplace_back("/50000.csv");
+  output_streams.emplace_back("/100000.csv");
+  output_streams.emplace_back("/500000.csv");
+  output_streams.emplace_back("/1000000.csv");
+  output_streams.emplace_back("/5000000.csv");
 
-  vector<int> integers = {100, 500, 1000, 5000, 10000, 25000, 50000, 100000, 250000, 500000, 750000, 1000000, 5000000};
+  vector<int> integers = {100, 500, 1000, 5000, 10000, 50000, 100000, 500000, 1000000, 5000000};
 
   while (!integers.empty()) {
     auto outputStreamForFind = ofstream(pathToFind + output_streams.front(), ios::ios_base::trunc);
@@ -45,24 +42,24 @@ int main() {
 
     const auto seed = chrono::system_clock::now().time_since_epoch().count();
     auto engine = mt19937(seed);  // без seed`а генератор будет выдавать одни и те же значения
-    auto dist = uniform_int_distribution(0, 100000);  // равновероятное распределение генерируемых чисел
+    auto dist = uniform_int_distribution(0, 10000);  // равновероятное распределение генерируемых чисел
 
     if (outputStreamForFind) {
-      for (int counter = 0; counter < integers.front(); counter++) {
+      for (int counter = 0; counter < integers.front()-1; counter++) {
         outputStreamForFind << dist(engine) << ',';
       }
       outputStreamForFind << dist(engine) << '\n';
     }
 
     if (outputStreamForErase) {
-      for (int counter = 0; counter < integers.front(); counter++) {
+      for (int counter = 0; counter < integers.front()-1; counter++) {
         outputStreamForErase << dist(engine) << ',';
       }
       outputStreamForErase << dist(engine) << '\n';
     }
 
     if (outputStreamForInsert) {
-      for (int counter = 0; counter < integers.front(); counter++) {
+      for (int counter = 0; counter < integers.front()-1; counter++) {
         outputStreamForInsert << dist(engine) << ',';
       }
       outputStreamForInsert << dist(engine) << '\n';
