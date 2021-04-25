0b07a2b852501d936f611e473d319c9a5d95395e
Semen Sokolov <simionsimion@yandex.ru>
2021-04-25 21:37:19 +0300

    Update bench

--- benchmark/find_max_and_min_benchmark.cpp
+++ benchmark/find_max_and_min_benchmark.cpp
@@ -20,23 +20,21 @@ static constexpr auto kDatasetPath = string_view{PROJECT_DATASET_DIR_FIND};
 static constexpr auto kProjectPath = string_view{PROJECT_SOURCE_DIR};
 
 int main() {
-
-
   // Tip 1: входные аргументы позволяют более гибко контролировать параметры вашей программы
   const auto path = string(kDatasetPath);
-  const auto output_path = string(kProjectPath) + "/benchmark/result/findResults.csv";
+  const auto output_path = string(kProjectPath) + "/benchmark/result/findMaxAndMinResults.csv";
 
   auto output_file1 = ofstream(output_path);
   output_file1.close();
   // работа с набором данных
-  vector <string> files = {"100", "500", "1000", "5000", "10000", "50000", "100000", "500000", "1000000", "5000000"};
+  vector<string> files = {"100", "500", "1000", "5000", "10000", "50000", "100000", "500000", "1000000", "5000000"};
   AVLTree<int> avlTree;
 
-  for (string file : files) { // Проходим по всем 10 .csv файлам
-    for (int i = 1; i < 11; i++) { // Запускаем замерку времени 10 раз
+  for (string file : files) {       // Проходим по всем 10 .csv файлам
+    for (int i = 1; i < 11; i++) {  // Запускаем замерку времени 10 раз
       string line = "1";
       auto input_file = ifstream(path + "/" + file + ".csv");
-      auto time_diff_remove = chrono::nanoseconds::zero();
+      auto time_diff_find_max_min = chrono::nanoseconds::zero();
       cout << (path + "/" + file) << endl;
 
       if (input_file) {
@@ -49,31 +47,22 @@ int main() {
       }
       input_file.close();
 
-      line = "1";
-      input_file = ifstream(path + "/" + file + ".csv");
+      //         здесь находится участок кода, время которого необходимо замерить
 
-//         здесь находится участок кода, время которого необходимо замерить
-      if (input_file) {
-        int line_number = 0;
-        for (string line; getline(input_file, line, ','); /* ... */) {
-          auto ss = stringstream(line);
-          const auto time_point_before_remove = chrono::steady_clock::now();
-          avlTree.find(stoi(line));
-          const auto time_point_after_remove = chrono::steady_clock::now();
-          line_number++;
-          time_diff_remove += time_point_after_remove - time_point_before_remove;
-        }
-      }
+      const auto time_point_before_find_max_min = chrono::steady_clock::now();
+      avlTree.find_max();
+      avlTree.find_min();
+      const auto time_point_after_find_max_min = chrono::steady_clock::now();
+      time_diff_find_max_min += time_point_after_find_max_min - time_point_before_find_max_min;
 
-      const auto time_elapsed_ns_erase = chrono::duration_cast<chrono::nanoseconds>(time_diff_remove).count();
+      const auto time_elapsed_ns_erase = chrono::duration_cast<chrono::nanoseconds>(time_diff_find_max_min).count();
       cout << time_elapsed_ns_erase << endl;
 
       avlTree.clear();
-      input_file.close();
 
       //Открываем файл для записи и вносим полученые данные
       auto output_file = fstream(output_path, ios::app);
-      output_file << file << "," <<  i << "," << time_elapsed_ns_erase << endl;
+      output_file << file << "," << i << "," << time_elapsed_ns_erase << endl;
       output_file.close();
     }
   }
--- main.cpp
+++ main.cpp
@@ -3,17 +3,17 @@
 #include "AVLTree.hpp"
 using namespace itis;
 using namespace std;
-  int main(int /*argc*/, char* /*argv*/[]) {
-    AVLTree<std::string> tree;
-    tree.insert("a");
-    tree.insert("b");
-    tree.insert("c");
-    tree.insert("d");
-    tree.insert("e");
-    tree.insert("f");
-    tree.insert("g");
+int main(int /*argc*/, char* /*argv*/[]) {
+  AVLTree<std::string> tree;
+  tree.insert("a");
+  tree.insert("b");
+  tree.insert("c");
+  tree.insert("d");
+  tree.insert("e");
+  tree.insert("f");
+  tree.insert("g");
 
-    tree.display();
-    cout<<tree.find_max();
-    return 0;
-  }
+  tree.display();
+  cout << tree.find_max();
+  return 0;
+}
